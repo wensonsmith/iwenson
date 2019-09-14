872d6ae8a4b7309b229a2528c1689e54ebf24c55
WensonSmith <wensonsmith@gmail.com>
2019-09-14 14:49:38 +0800

    feat: archive

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -10,26 +10,41 @@ module.exports = {
         logo: '/seeker_logo_white.png',
         footer: {
             slogan: '世界你好',
-            // copyright: 'no copyright'
+            // copyright: 'no copyright',
+            social: [
+                {
+                    type: 'qq', // qq, wechat, bilibili, github, rss, weibo
+                    value: '', 
+                },
+                {
+                    type: 'wechat', // qq, wechat, bilibili, github, rss, weibo
+                    value: '', 
+                },
+            ]
         },
         watchDog: 'cat',
         nav: [
             {
                 text: '归档',
-                link: '/archive'
+                link: '/archive/'
             },
             {
                 text: '分类',
-                link: '/category'
-            },
-            {
-                text: '标签',
-                link: '/tags'
+                link: '/categories/'
             },
             {
                 text: 'Github',
                 link: 'https://github.com/wensonsmith'
             },
         ],
+    },
+    configureWebpack() {
+        return {
+            resolve: {
+                alias: {
+                    '@public': path.join(__dirname, './public')
+                }
+            }
+        }
     }
 }
